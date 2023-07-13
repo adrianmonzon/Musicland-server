17e67cb154a5d48af7bedc9b725cdfdeabca0ad9
Adrián MF <adrianmf@MacBook-Pro-de-Adrian.local>
2023-07-14 01:01:14 +0200

    nodemailer config updated

--- configs/nodemailer.config.js
+++ configs/nodemailer.config.js
@@ -1,15 +1,15 @@
 const nodemailer = require('nodemailer')
 
 let transporter = nodemailer.createTransport({
-    // service: 'Gmail',
-    host: 'smtp.servidor-correo.net',
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAILUSER,
-        pass: process.env.EMAILPWD
-    }
-})
+  service: "Gmail",
+  // host: 'smtp.servidor-correo.net',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAILUSER,
+    pass: process.env.EMAILPWD,
+  },
+});
 
 console.log(transporter);
 module.exports = transporter
